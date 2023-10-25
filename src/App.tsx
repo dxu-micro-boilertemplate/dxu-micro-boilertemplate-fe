c0776c51a2382ec46d26005aec3236a583f4c3b0
minhductxqt <minhductxqt@gmail.com>
2023-10-25 15:47:39 +0700

    feat: get data users

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
+import type { User } from "./types/user";
+
 import "./index.css";
 
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [data, setData] = useState<User>();
+
+  const handleGetData = async () => {
+    try {
+      const data: User = await axios.get("http://localhost/api/v1/User");
+
+      if (data) {
+        setData(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    handleGetData();
+  }, []);
+
   return <></>;
 }
 
